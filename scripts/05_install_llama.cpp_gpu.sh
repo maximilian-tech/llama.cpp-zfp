0b07293e40cde75467383b9398c7629d97431f45
Maximilian Sander <maximilian.sander1@tu-dresden.de>
2025-04-13 12:07:23 +0200

    Add gpu build option

--- scripts/05_install_llama.cpp_gpu.sh
+++ scripts/05_install_llama.cpp_gpu.sh
@@ -13,12 +13,13 @@ source ${ROOT_DIR}/source_env.cpp_gpu
 cmake -B build_gpu \
   --install-prefix=${ROOT_DIR}/llama.cpp-gpu \
   -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=False \
   -DGGML_NATIVE=ON \
   -DGGML_LTO=ON \
   -DGGML_CUDA=ON \
   -DGGML_ZFP_ENABLE=OFF \
   -DGGML_ZFP_MODE="" \
-  -DCMAKE_CUDA_COMPILER=nvcc \
+  -DCMAKE_CUDA_ARCHITECTURES="90" \
   --fresh
 
 make -C build_gpu -j 8 
